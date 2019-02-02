a6bda8257709566a852e4345b07cfb46a407d1f4
Mircea Bucerzan <mircea@Mirceas-MacBook-Pro.local>
2019-02-02 22:27:54 +0200

    Created base app theme
    - Extended Material Components Theme
    - Defined values for base theme color attributes
    - Defined color palette
    - Updated material library version to 1.1.0-alpha03
    - Added Constraint Layout library
    
    Trello: https://trello.com/c/E3XlFdTY

---
+++ app/src/main/java/app/flavoury/FlavouryActivity.kt
@@ -0,0 +1,13 @@
+package app.flavoury
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FlavouryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flavoury)
+    }
+
+}
